0e2c99031abcd72b9aa6040ec76a0ce2174a959b
James <3richj71@solent.ac.uk>
2018-10-30 02:40:47 +0000

    Created gui for week 5

---
+++ myPracticeCourseWork/week5gui/hotellock-parent/lock-reception/src/main/java/solent/ac/uk/ood/examples/hotellock/reception/Controller.java
@@ -0,0 +1,75 @@
+package solent.ac.uk.ood.examples.hotellock.reception;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import solent.ac.uk.ood.examples.hotellock.model.CardKey;
+import solent.ac.uk.ood.examples.hotellock.model.HotelReceptionService;
+import solent.ac.uk.ood.examples.hotellock.model.SecretKeyProvider;
+import solent.ac.uk.ood.examples.hotellock.secretkey.SecretKeyProviderImpl;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Controller {
+
+    //Create Card Elements
+    public Button createCard;
+    public TextField ccRoomNumber;
+    public TextField ccValidFrom;
+    public TextField ccExpirationDate;
+    public TextField ccCardCode;
+
+    //Check Card Elements
+    public Button checkCard;
+    public TextField checkCardCode;
+    public Label checkRomNumber;
+    public Label checkValidFrom;
+    public Label checkExpirationDate;
+
+    HotelReceptionService hotelReceptionService = new HotelReceptionServiceImpl();
+    SecretKeyProvider secretKeyProvider = new SecretKeyProviderImpl();
+
+    public void guiCreateCardCode() {
+
+        hotelReceptionService.setSecretKeyProvider(secretKeyProvider);
+
+        // used to parse dates
+        DateFormat df = new SimpleDateFormat("dd/MM/yy HH:mm");
+
+        String roomNumber = ccRoomNumber.getText();
+        Date setStartDate = null;
+        try {
+            setStartDate = df.parse(ccValidFrom.getText());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        Date setEndDate = null;
+        try {
+            setEndDate = df.parse(ccExpirationDate.getText());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        String cardCode = hotelReceptionService.createCardCode(roomNumber, setStartDate, setEndDate);
+        ccCardCode.setText(cardCode);
+    }
+
+
+    public void guiCheckCardCode() {
+        hotelReceptionService.setSecretKeyProvider(secretKeyProvider);
+
+        String cardCode = checkCardCode.getText();
+
+        CardKey cardKey = hotelReceptionService.readCard(cardCode);
+
+        checkRomNumber.setText(cardKey.getRoomNumber());
+        checkValidFrom.setText(cardKey.getStartDate().toString());
+        checkExpirationDate.setText(cardKey.getEndDate().toString());
+    }
+
+
+}
